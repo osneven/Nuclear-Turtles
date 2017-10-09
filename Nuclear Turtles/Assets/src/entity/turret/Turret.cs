ccf2b3188dce77b77de84770664d45d4d13f59d7
osneven <oliver@neven.dk>
2017-10-09 14:56:28 +0200

    added basic layout for enties and turtles

--- Nuclear Turtles/Assets/src/entity/turret/Turret.cs
+++ Nuclear Turtles/Assets/src/entity/turret/Turret.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Turtle : MonoBehaviour {
+public class Turret : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ Nuclear Turtles/Assets/src/entity/turtle/Turtle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turtle : Entity {
+	
+	public Turtle(Vector2 position) : base(position) {
+		
+	}
+
+	public override void Start() {
+		
+	}
+
+	public override void Update() {
+		
+	}
+}
