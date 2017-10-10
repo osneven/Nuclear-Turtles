80e61c53ba903e8f6e3e15db14609b2f2f53043d
osneven <oliver@neven.dk>
2017-10-10 09:11:44 +0200

    Unpushed work from yesterday

--- Nuclear Turtles/Assets/src/entity/turtle/Turtle.cs
+++ Nuclear Turtles/Assets/src/entity/turtle/Turtle.cs
@@ -4,16 +4,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Turtle : Entity {
+
+	private Level level;
+	public float speed;
+	private int waypointIndex;
+	private Vector2 velocity, waypoint;
 	
-	public Turtle(Vector2 position) : base(position) {
-		
-	}
+	public Turtle(Vector2 position) : base(position) {}
 
 	public override void Start() {
-		
+		// Get the level script from the level object
+		level = GameObject.Find("level").GetComponent<Level>();
+
+		// Get the first waypoint
+		waypointIndex = 0;
 	}
 
 	public override void Update() {
-		
+		// Move the turtle
+		Move();
+	}
+
+	private void Move() {
+		// Move towards the waypoint
+
+
+		// Check if waypoint has been reached
+		if ((Vector2)this.transform.position == waypoint) {
+			Vector2 oldWaypoint = waypoint;
+
+			// Check if it's the last waypoint
+			if (level.IsFinalWaypoint(waypoint)) {
+				// TODO: DECREASE HP OF THE PLAYER
+			}
+
+			// Otherwise, get the next waypoint
+			else {
+				waypoint = level.NextWaypoint(waypointIndex);
+				waypointIndex++;
+			}
+
+			// Turn the velocity towards the new waypoint
+			velocity = Vector2.Angle()
+		}
 	}
 }
---
+++ Nuclear Turtles/Assets/src/level/Level.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+	private Vector2[] waypoints;
+
+	// Use this for initialization
+	void Start () {
+
+		// Get the waypoints
+		waypoints = FindWaypoins();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	private Vector2[] FindWaypoins() {
+
+		// Get all waypoints
+		GameObject[] waypointObjects = GameObject.FindGameObjectsWithTag("Waypoint");
+
+		// Get all the positions
+		Vector2[] waypoints = new Vector2[waypointObjects.Length];
+		for (int i = 0; i < waypointObjects.Length; i ++) {
+			Vector3 pos = waypointObjects[i].transform.position;
+			waypoints[i] = new Vector2(pos.x, pos.y);
+		}
+
+		return waypoints;
+	}
+
+	public Vector2 NextWaypoint(int index) {
+		return waypoints[index + 1];
+	}
+	public Vector2 FirstWaypoint() {
+		return waypoints[0];
+	}
+	public bool IsFinalWaypoint(Vector2 waypoint) {
+		return waypoint == waypoints[waypoints.Length - 1];
+	}
+
+}
